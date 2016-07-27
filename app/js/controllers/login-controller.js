17451aeb29705ea4954c56c419179b64880f86fa
Bob Zurad <bob@zurad.net>
2016-07-26 22:45:27 -0500

    implemented login function

--- app/js/controllers/login-controller.js
+++ app/js/controllers/login-controller.js
@@ -1,12 +1,16 @@
 angular
   .module('NoteFire')
   .controller('LoginController', [
-    '$location', 'currentAuth',
-    function($location, currentAuth) {
+    '$location', 'Auth',
+    function($location, Auth) {
       'use strict';
 
       var controller = this;
 
+      //if user is already authenticated, redirect them home
+      if (Auth.$getAuth().uid) {
+        $location.url('home');
+      }
       //hack to make bootstrap tabs work with Angular
       angular.element('#loginTabs a').click(function (e) {
         e.preventDefault();
@@ -14,7 +18,21 @@ angular
       });
 
       controller.signin = function() {
-        
+        controller.error = null;
+
+        Auth.$signInWithEmailAndPassword(controller.email, controller.password)
+          .then(function(user) {
+            $location.url("home");
+          })
+          .catch(function(error) {
+            if (error.code === "auth/user-not-found") {
+              controller.error = "Email Address Not Found";
+            } else if (error.code === "auth/wrong-password"){
+              controller.error = "Invalid Password";
+            } else {
+              controller.error = error.message;
+            }
+          });
       };
 
       controller.register = function() {
