3789ac0bd2929f8cad4963738ab7e01fe95a8467
Bob Zurad <bob@zurad.net>
2016-08-14 21:38:06 -0500

    stripped HTML from note cards on home page

--- app/js/controllers/public-controller.js
+++ app/js/controllers/public-controller.js
@@ -10,7 +10,18 @@ angular
       angular.element("#newNoteLink").show();
 
       PublicNoteService.init();
-      controller.notes = PublicNoteService.getNotes();
+      PublicNoteService.getNotes()
+        .$loaded()
+        .then(function(notes) {
+          controller.notes = notes.map(function(note) {
+            return {
+              id: note.$id,
+              dateCreated: note.dateCreated,
+              title: note.title,
+              content: note.content.replace(/<\/?[^>]+>/gi, '').substr(0, 300)
+            };
+          });
+        });
 
       controller.onCardClick = function(id) {
         $location.url('view/' + id);
